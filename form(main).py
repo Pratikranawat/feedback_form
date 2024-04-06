92295b73bb1e6d295c99198809ba6d0559368663
Pratik Ranawat <161489159+Pratikranawat@users.noreply.github.com>
2024-04-06 10:41:24 +0000

    s_p

--- form(main).py
+++ form(main).py
@@ -1,4 +1,4 @@
-print("Feedback Form")
+print("---------------Feedback Form------------------")
 #
 first_name=input("first name: ")
 if(first_name.isdigit()):
@@ -16,10 +16,19 @@ contact=input("contact number:")
 #
 enroll_id=input("enrollment no:")
 #
-school==input("school:")
-if(school.isdigit()):
-    print("invalid input")
-    last_name=input("last name: ")
+print("select school : ")
+print("1.SOE\n2.SOM\n3.SOD\n4.SOL")
+school = int(input("-: "))
+if school ==1:
+    print("SOE")
+if school ==2:
+    print("SOM")
+if school ==3:
+    print("SOD")
+if school ==4:
+    print("SOL")
+else :
+    print("ENTER A VALID NUMBER ")
 #
 programme=input("programme:")
 if(programme.isdigit()):
